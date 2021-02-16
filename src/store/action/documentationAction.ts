20c6f577e18fdd74df6ab853631caba2177f36cf
Vincent Kollin <vincent.kollin@check24.de>
2021-02-16 14:23:50 +0100

    remove unused

--- src/store/action/documentationAction.ts
+++
@@ -1,7 +0,0 @@
-import {ThunkActionType, ThunkDispatchType} from "../../type/thunk";
-
-export function fetchOpenApiDoc(): ThunkActionType<Promise<JSON>> {
-    return (dispatch: ThunkDispatchType, getState, {documentationRepository}): Promise<JSON> => {
-        return documentationRepository.fetchOpenApiSpecs();
-    };
-}
