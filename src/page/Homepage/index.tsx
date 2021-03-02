353ee955ba765a9337074fbdafaecb8d670a89d3
Vincent Kollin <vincent.kollin@check24.de>
2021-03-02 07:32:08 +0100

    Add info text to the homepage

--- src/page/Homepage/index.tsx
+++ src/page/Homepage/index.tsx
@@ -7,7 +7,48 @@ import s from './index.scss';
 export default (): JSX.Element => {
     return <PageContent>
         <h1>Umfrage</h1>
-        <p>[Platzhalter]</p>
-        <Link className={s.Button} to={Routes.FAN_LOCATION_INTERVIEW}>Zur Umfrage</Link>
+
+        <div className={s.ButtonWrapper}>
+            <Link className={s.Button} to={Routes.FAN_LOCATION_INTERVIEW}>Direkt zur Umfrage … </Link>
+        </div>
+
+        <p><i>… oder noch ein bisschen lesen</i></p>
+
+        <p>Wir haben diese Umfrage gebaut, um auf einer Karte darzustellen in welche Regionen (nach Postleitzahl
+            gruppiert) es Fans von verschiedenen Klubs verschlagen hat.</p>
+
+        <p>Es gibt ähnliche Karten bei diversen regionalen Tageszeitungen zu Vereinsmitgliedern von großen Klubs. Jedoch
+            keine einzelne Karte mit allen. Zudem sind kleinere Klubs nie vertreten und auch nicht jeder ist
+            Vereinsmitglied.</p>
+
+        <p>Hier wollen wir versuchen, eine Karte zu erstellen, auf der sich alle Klubs wiederfinden.</p>
+
+        <p>Zusätzlich könnt ihr auch eure Hassvereine angeben (der ein oder andere hat da ja bestimmt ein paar 😉). Die
+            Ergebnisse hierzu werden später auch grafisch aufbereitet.</p>
+
+        <p>In der Anfangsphase wird noch keine Karte mit den Ergbnissen zu sehen sein, da es für uns keinen Sinn macht,
+            die wenigen Daten zu zeigen, da sie kaum aussagekräftig sein werden.</p>
+
+        <p>Zwischenzeitlich werden wir euch aber auf <a href="https://turus.net">turus.net</a>, Facebook und Co.
+            berichten, wie es vorangeht und wann die Ergebnisse zu sehen sein werden.</p>
+
+        <p>Wir sind schon super gespannt und freuen uns auf eure rege Teilnahme!</p>
+
+        <p>Danke!</p>
+
+        <p>
+            <i>
+                PS: Wir haben versucht jeden Klub bis in die unteren Ligen mit aufzunehmen. Da das ziemlich viele sind
+                (so an die 15.00!), verzeiht uns bitte, wenn einzelne fehlen sollten. Aus diesem Grund sind auch nur
+                deutsche Klubs vertreten. <br/>Solltet ihr euren Klub nicht in der Liste finden, würden wir uns freuen,
+                wenn ihr euch <a href="mailto:turus@devultras.com">meldet</a>. Wir werden anschließend den Klub mit
+                aufnehmen.
+            </i>
+        </p>
+
+        <div className={s.ButtonWrapper}>
+            <Link className={s.Button} to={Routes.FAN_LOCATION_INTERVIEW}>Zur Umfrage</Link>
+        </div>
+
     </PageContent>
 }
